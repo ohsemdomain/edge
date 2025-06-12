784794753a2a00c93211358716b7172c7e9cc398
ohsemdomain <ohsemdomain@gmail.com>
2025-06-13 02:09:31 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from "node:path";
-import { cloudflare } from "@cloudflare/vite-plugin"
+import path from 'node:path'
+import { cloudflare } from '@cloudflare/vite-plugin'
 import tailwindcss from '@tailwindcss/vite'
 
-
 export default defineConfig({
-  plugins: [react(), tailwindcss(), cloudflare()],
-  server: {
-    port: 5090,
-    watch: {
-      ignored: ['**/.wrangler/**', '**/node_modules/**']
-    }
-  },
-  resolve: {
-    alias: {
-      '@shared': path.resolve(__dirname, '-shared'),
-      '@app': path.resolve(__dirname, '-app'),
-      '@worker': path.resolve(__dirname, '-worker')
-    },
-  }
+	plugins: [react(), tailwindcss(), cloudflare()],
+	server: {
+		port: 5090,
+		watch: {
+			ignored: ['**/.wrangler/**', '**/node_modules/**']
+		}
+	},
+	resolve: {
+		alias: {
+			'@shared': path.resolve(__dirname, '-shared'),
+			'@app': path.resolve(__dirname, '-app'),
+			'@worker': path.resolve(__dirname, '-worker')
+		}
+	}
 })
