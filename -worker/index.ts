ebcf98b56f1afd26ec01d245ce709944e2f81e27
Ohsem Domain <ohsemdomain@gmail.com>
2025-06-13 01:43:31 +0800

    Init

---
+++ -worker/index.ts
@@ -0,0 +1,7 @@
+export default {
+  async fetch(request: Request): Promise<Response> {
+    return new Response("Hello World!", {
+      headers: { "Content-Type": "text/plain" }
+    });
+  }
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from "node:path";
+import { cloudflare } from "@cloudflare/vite-plugin"
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss(), cloudflare()],
+  server: {
+    port: 5090,
+    watch: {
+      ignored: ['**/.wrangler/**', '**/node_modules/**']
+    }
+  },
+  resolve: {
+    alias: {
+      '@shared': path.resolve(__dirname, '-shared'),
+      '@app': path.resolve(__dirname, '-app'),
+      '@worker': path.resolve(__dirname, '-worker')
+    },
+  }
 })
