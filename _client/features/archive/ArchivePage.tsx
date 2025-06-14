d3ed6b109b3972f1757d943b660158c38928a1f1
ohsemdomain <ohsemdomain@gmail.com>
2025-06-14 12:57:43 +0800

    Refactor ArchivePage component: Update layout to use Flex and Box for improved responsiveness, replace Stack with Tabs for better organization, and add ScrollArea for ItemsArchive panel.

--- _client/features/archive/ArchivePage.tsx
+++ _client/features/archive/ArchivePage.tsx
@@ -1,31 +1,31 @@
-import { Stack, Tabs, Text, Title } from '@mantine/core'
+import { Box, Flex, ScrollArea, Tabs } from '@mantine/core'
 import { ContactsArchive } from './ContactsArchive'
 import { ItemsArchive } from './ItemsArchive'
 
 export function ArchivePage() {
 	return (
-		<Stack>
-			<div>
-				<Title order={2}>Archive</Title>
-				<Text c='dimmed' size='sm'>
-					Inactive records
-				</Text>
-			</div>
+		<Flex h='100%' style={{ justifyContent: 'center', overflow: 'hidden' }}>
+			<Box maw={800} w='100%' h='100%' p='md'>
+				<Tabs
+					defaultValue='contacts'
+					style={{ display: 'flex', flexDirection: 'column', height: '100%' }}
+				>
+					<Tabs.List>
+						<Tabs.Tab value='contacts'>Contacts</Tabs.Tab>
+						<Tabs.Tab value='items'>Items</Tabs.Tab>
+					</Tabs.List>
 
-			<Tabs defaultValue='contacts'>
-				<Tabs.List>
-					<Tabs.Tab value='contacts'>Contacts</Tabs.Tab>
-					<Tabs.Tab value='items'>Items</Tabs.Tab>
-				</Tabs.List>
+					<Tabs.Panel value='contacts' pt='md' style={{ flex: 1, overflow: 'hidden' }}>
+						<ContactsArchive />
+					</Tabs.Panel>
 
-				<Tabs.Panel value='contacts' pt='md'>
-					<ContactsArchive />
-				</Tabs.Panel>
-
-				<Tabs.Panel value='items' pt='md'>
-					<ItemsArchive />
-				</Tabs.Panel>
-			</Tabs>
-		</Stack>
+					<Tabs.Panel value='items' pt='md' style={{ flex: 1, overflow: 'hidden' }}>
+						<ScrollArea h='100%'>
+							<ItemsArchive />
+						</ScrollArea>
+					</Tabs.Panel>
+				</Tabs>
+			</Box>
+		</Flex>
 	)
 }
